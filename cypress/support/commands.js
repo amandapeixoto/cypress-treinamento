bbd523ea5b4eee67034129ba17d8de0ea70fd8f8
amandapeixoto <amandapeixoto.ap1@gmail.com>
2025-04-20 10:59:09 -0300

    adiciona comando para criação automática de usuário com Faker

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,48 @@
+import { faker } from "@faker-js/faker";
+
+Cypress.Commands.add('criarUsuario', () => {
+    const nome = faker.person.firstName();
+    const email = faker.internet.email();
+    const senha = faker.internet.password();
+
+    // Salva os dados no arquivo
+    cy.writeFile('cypress/fixtures/usuario.json', {
+        nome,
+        email,
+        senha
+      });
+
+    Cypress.env('usuarioEmail', email);
+    Cypress.env('usuarioSenha', senha);
+
+    cy.visit('https://automationexercise.com');
+    cy.contains('Signup / Login').click();
+    cy.get('[data-qa="signup-name"]').type(nome);
+    cy.get('[data-qa="signup-email"]').type(email);
+    cy.get('[data-qa="signup-button"]').click();
+
+    cy.get('#id_gender2').check();
+    cy.get('[data-qa="password"]').type(senha);
+    cy.get('[data-qa="days"]').select('15');
+    cy.get('[data-qa="months"]').select('September');
+    cy.get('[data-qa="years"]').select('1999');
+    cy.get('#newsletter').check();
+    cy.get('#optin').check();
+
+    cy.get('[data-qa="first_name"]').type(nome);
+    cy.get('[data-qa="last_name"]').type(faker.person.lastName());
+    cy.get('[data-qa="address"]').type(faker.location.streetAddress());
+    cy.get('[data-qa="country"]').select('Australia');
+    cy.get('[data-qa="state"]').type(faker.location.state());
+    cy.get('[data-qa="city"]').type(faker.location.city());
+    cy.get('[data-qa="zipcode"]').type(faker.location.zipCode());
+    cy.get('[data-qa="mobile_number"]').type(faker.phone.number());
+
+    cy.get('[data-qa="create-account"]').click();
+    cy.contains('Account Created!').should('be.visible');
+    cy.get('[data-qa="continue-button"]').click();
+}) 
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
